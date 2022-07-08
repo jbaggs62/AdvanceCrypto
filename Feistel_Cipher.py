a828cefaef73db5d3e188247c296b3168916f445
Jake <36750670+jbaggs62@users.noreply.github.com>
2022-07-08 17:08:47 +0000

    pushing up changes

--- Feistel_Cipher.py
+++ Feistel_Cipher.py
@@ -1,45 +1,63 @@
 import numpy as np
-from Feistel_encoder import encoder_function
+from Feistel_encoder import enc_dec_round
+from text_to_binary import func_0, func_1, func_2
 from List_of_Charcter_to_ascii import string_to_ascii
 from ascii_to_binary import ascii_to_binary
 from  string_to_list_and_reverse import string_to_list_or_vice_versa
 
 
 
-user_input = "hello world"
-def Feistel_cipher(user_input:str, key:list):
-  # convert input to string 
-  string_input = string_to_list_or_vice_versa(user_input)
-  int_input = string_to_ascii(string_input)
-  binary_input = ascii_to_binary(int_input, 'nonbinary')
-  # create list to append encoded values in
-  list_of_encoded = []
-  binary_keys = []
-  for keys in key:
-    binary_keys.append(np.binary_repr(keys, width=4))
-  # for each item in my input I want to loop through
-  encrypted_char = []
-  for item in binary_input:
-    temp_value = item
-    for key in binary_keys:
-      temp_value = encoder_function(temp_value,key)
-    temp_cipher_char_swap = temp_value[4:]+temp_value[:4] 
-    encrypted_char.append(temp_cipher_char_swap)
-  output = ascii_to_binary(list_of_encoded, 'binary')
-  output = string_to_ascii(output)
-  output = string_to_list_or_vice_versa(output)
-  return output
-
-
+#main feistel cipher function
+def feistel_cipher(message, key):
+  #initialize the key value and convert the key to 4 dig binary
+  all_key_int = key
+  all_key_bin = []
+  for i in all_key_int:
+    all_key_bin.append(np.binary_repr(i,width=4))
+  #convert message string to 8 bit binary
+  message_char = func_0(message)
+  message_char_int = func_1(message_char)
+  message_char_bin = func_2(message_char_int)
+  #encryption round
+  cipher_char_bin = []
+  for i in message_char_bin:
+    temp_cipher_char = i
+    for j in all_key_bin:
+        temp_cipher_char = enc_dec_round(temp_cipher_char,j)
+    temp_cipher_char_swap = temp_cipher_char[4:]+temp_cipher_char[:4] 
+    cipher_char_bin.append(temp_cipher_char_swap)
+  #convert 8 bit binary cipher to string
+  cipher_char_int = func_2(cipher_char_bin)
+  cipher_char = func_1(cipher_char_int)
+  cipher_string = func_0(cipher_char)
+  return cipher_string
 #test 1
 #16 keys = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 key = np.arange(16)
 key_reverse = np.flip(key)
 message_1 = 'hello world'
-cipher_1 = Feistel_cipher(message_1, key)
-recovered_message_1 = Feistel_cipher(cipher_1, key_reverse)
+cipher_1 = feistel_cipher(message_1, key)
+recovered_message_1 = feistel_cipher(cipher_1, key_reverse)
 print(f'\ntest 1: ')
 print(f'key: {key}')
 print(f'original message: {message_1}')
 print(f'cipher text: {cipher_1}')
 print(f'recovered message: {recovered_message_1}')
+#test 2
+#16 keys = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+key = np.array([3,4,9,12,8,7])
+key_reverse = np.flip(key)
+message = 'this is test 2'
+cipher = feistel_cipher(message, key)
+recovered_message = feistel_cipher(cipher, key_reverse)
+print(f'\ntest 2: ')
+print(f'key: {key}')
+print(f'original message: {message}')
+print(f'cipher text: {cipher}')
+print(f'recovered message: {recovered_message}')
+#test 3
+#16 keys = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+key = np.array([7,7,8,8,9,9,10,10])
+key_reverse = np.flip(key)
+message = 'this is the third test'
+cipher = feistel_cipher(message, key)
--- Feistel_encoder.py
+++ Feistel_encoder.py
@@ -1,21 +1,20 @@
 import numpy as np
-
 #encryption and decryption round
-def encoder_function(input:str, key):
+def enc_dec_round(input_string, key):
 #split the L0 and R0
-  Left_0 = input[:4]
-  Right_0 = input[4:]
-  #convert to int from binary
-  Right_0 = int(Right_0,2)
-  key = int(key,2)
-  Left_0 = int(Left_0,2)
-  feistel_encryption_output= 2*(Right_0**key) %(2**4)
-  #xor 
-  xor_int= Left_0 ^ feistel_encryption_output
-  xor_binary = np.binary_repr(xor_int,width=4)
+  L_0 = input_string[:4]
+  R_0 = input_string[4:]
+  #convert binary string to integer and compute the feistel function
+  key_int = int(key,2)
+  L_0_int = int(L_0,2)
+  R_0_int = int(R_0,2)
+  function_output_int = 2*(R_0_int**key_int)%(2**4)
+  #function_output_binary = bin(function_output)[2:]
+  #xor the L0 and the output of the feistel function
+  xor_result_int = L_0_int^function_output_int
+  xor_result_binary = np.binary_repr(xor_result_int,width=4)
   #output L1 and R1
-  Right_0 = str(Right_0)
-  L_1 = Right_0
-  R_1 = xor_binary
-  output = L_1+R_1
-  return output
+  L_1 = R_0
+  R_1 = xor_result_binary
+  output_bin_string = L_1+R_1
+  return output_bin_string
